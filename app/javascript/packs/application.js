71de25ddb16f5ce38aa74b6dc23267d6edc6cfb5
Peeyush <peeyushgoyal154@gmail.com>
2021-03-11 11:25:54 +0530

    Basic JS integration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,12 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import "stylesheets/application";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+let homeIndexPage = () => {
+  console.log("Hello Home PAge");
+};
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+let menusIndexpage = () => {
+  console.log("Hello to menus!");
+};
 
-import "stylesheets/application"
+window.homeIndexPage = homeIndexPage;
+window.menusIndexpage = menusIndexpage;
